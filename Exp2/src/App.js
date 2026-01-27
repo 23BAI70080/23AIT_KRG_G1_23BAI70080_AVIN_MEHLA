a3f8ad8007c9a6a5f17d869883f567e851d87501
23BAI70080 <avinmehla1209@gmail.com>
2026-01-27 10:10:32 +0530

    Add files via upload

---
+++ Exp2/src/App.js
@@ -0,0 +1,5 @@
+import { BrowserRouter } from "react-router-dom";
+import Login from "./pages/login";
+import Logs from "./pages/logs";
+import DashboardAnalytics from "./pages/DashboardAnalytics";
+import DashboardAnalytics from "./pages/DashboardAnalytics";
